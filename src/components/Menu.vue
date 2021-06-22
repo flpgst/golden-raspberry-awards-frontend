cb17002a93d0276db12e4ec38cc576ad0a8ce8b8
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-06-21 23:24:44 -0300

    Create App Menu

---
+++ src/components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-navigation-drawer
+    clipped
+    permanent
+    app
+    >
+      <v-list
+        nav
+        dense
+      >
+
+          <v-list-item to="/">
+            <v-list-item-title >Dashboard</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item to="/movies">
+            <v-list-item-title >List</v-list-item-title>
+          </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+</template>
